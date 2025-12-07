0b5d2b83679ee9dfd88e253be79a718a4904a6a4
Kestroy <161330387+KestroyStephan@users.noreply.github.com>
2025-12-07 16:56:43 +0530

    Feat: Initialize app.js with API configuration

--- client/app.js
+++ client/app.js
@@ -1,1 +1,5 @@
-//app.js file created in client folder
+// client/app.js
+
+const API_BASE_URL = window.APP_CONFIG.API_BASE_URL;
+// Change this to match your Backend Key
+const CLIENT_API_KEY = "dev-demo-api-key-123"; 
