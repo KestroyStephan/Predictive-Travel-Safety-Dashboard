52cf6cdc96b0a1e72d8a6a0d6079ac54a2dbaa66
Kestroy <161330387+KestroyStephan@users.noreply.github.com>
2025-12-07 21:33:47 +0530

    Initialize server with necessary imports and configurations

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-// server.js file created in server folder
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import session from "express-session";
+import passport from "passport";
+import axios from "axios"; 
+
+import { AdvisoryRecord } from "./models/AdvisoryRecord.js";
+import authRouter, {
+  configurePassport,
+  requireAuth,
+  requireApiKey
+} from "./auth.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/travel_safe";
